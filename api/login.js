3469eab56e1ae14ba18866cf9de80af35963af50
Andrew Chen <and.chenn@gmail.com>
2021-12-15 18:49:48 -0800

    fixed formatting and router issues

--- api/login.js
+++ api/login.js
@@ -1,10 +1,8 @@
+var express = require('express')
+var router = express.Router()
+var pool = require("../server");
 
-const Router = require("express");
-var router = Router();
-var pool = require("../../server");
-router.post('/', function (req, resp) {
-
-
+router.post('/login', function (req, resp) {
     var text = 'SELECT * FROM users WHERE username = $1 AND password = $2;'
     var values = [req.body.username, req.body.password]
 
@@ -13,14 +11,10 @@ router.post('/', function (req, resp) {
         console.log(err, res.rows.length)
         if (res.rows.length != 0) {
             resp.send("success")
-
-
         } else {
             resp.send("failure")
-
-
         }
     })
-
 });
-module.export = router;
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var {Pool} = require('pg');
-var baserouter  = require("./routes/api/login")
+var apiRouter  = require("./api/login");
+
 var app = express();
 var pool = new Pool({
     user:'postgres',
     host:'localhost',
-    database: 'postgres',
+    database: 'testdb',
     password: 'admin',
     port: 5432
-
-
 })
 
 app.use(express.json());
@@ -20,10 +19,11 @@ var options = {
     extensions: ['htm', 'html'],
     index: 'index.html'
 }
-
 app.use('/', express.static('./pub_html', options));
-app.use('/', baserouter)
+
+app.use('/api', apiRouter)
 
 var PORT = 3000;
 app.listen(PORT, () => console.log("Server started on port " + PORT));
-module.export = pool;
+
+module.exports = pool;
